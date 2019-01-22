c6e1d754b1e0383694fc7a6a7b61f25bc2f15d91
Joshua Holcomb <A00303149@ad.tjc.local>
2019-01-22 13:26:02 -0600

    Modified function to use input params rather than hard coded variables.

--- Functions/Generate-ViewHygineEmail/Generate-ViewHygieneEmail.ps1
+++ Functions/Generate-ViewHygineEmail/Generate-ViewHygieneEmail.ps1
@@ -1,5 +1,23 @@
 function Generate-ViewHygieneEmail {
 
+    [CmdletBinding()]
+	Param(
+        [Parameter(Mandatory=$true)]
+            [String]$HorizonCredStore,
+        [Parameter(Mandatory=$true)]
+            [String]$SMTPServer,
+        [Parameter(Mandatory=$true)]
+            [String]$FromEmail,
+        [Parameter(Mandatory=$true)]
+            [Array]$To,
+        [Parameter(Mandatory=$true)]
+            [Array]$CC,
+        [Parameter(Mandatory=$true)]
+            [Array]$BCC,
+        [Parameter(Mandatory=$true)]
+            [String]$Subject
+	)
+
 <#
 	.SYNOPSIS
 	Generates a Email report of the problem VM's in the View Environment.
@@ -8,7 +26,7 @@ function Generate-ViewHygieneEmail {
 	The Add-HVDesktop adds virtual machines to already exiting pools by using view API service object(hvServer) of Connect-HVServer cmdlet. VMs can be added to any of unmanaged manual, managed manual or Specified name. This advanced function do basic checks for pool and view API service connection existance, hvServer object is bound to specific connection server.
 
 	.EXAMPLE
-		Generate-ViewHygieneEmail
+		Generate-ViewHygieneEmail -HorizonCredStore -SMTPServer -FromEmail -To -CC -BCC -Subject
 		Sends an email to the recipients specified in the script.
 
 	.NOTES
@@ -26,14 +44,7 @@ function Generate-ViewHygieneEmail {
 ##################################################################
 #            Variables                                           #
 ##################################################################
-    $SMTPServer = 'smtp.mydomain.com'
-    $FromEmail = 'View Admin Server <viewadmin@mydomain.com>'
-    $To = 'admin@mydomain.com'
-    $CC = ''
-    $BCC = ''
-    $Subject = 'View Admin Hygiene Report'
-	
-    $ProblemVMs = Get-HVProblemMachines
+    $ProblemVMs = Get-HVProblemMachines -HorizonCredStore $HorizonCredStore
 # End Variables
 
 ##################################################################
@@ -57,3 +68,52 @@ function Generate-ViewHygieneEmail {
 
 # End Main
 }
+
+# SIG # Begin signature block
+# MIIIeQYJKoZIhvcNAQcCoIIIajCCCGYCAQExCzAJBgUrDgMCGgUAMGkGCisGAQQB
+# gjcCAQSgWzBZMDQGCisGAQQBgjcCAR4wJgIDAQAABBAfzDtgWUsITrck0sYpfvNR
+# AgEAAgEAAgEAAgEAAgEAMCEwCQYFKw4DAhoFAAQUloQo9qGzup9uIMQ26lTJkV/8
+# 49ugggXOMIIFyjCCBLKgAwIBAgITFQAAByMkUpCwipe3DwAAAAAHIzANBgkqhkiG
+# 9w0BAQsFADBdMRUwEwYKCZImiZPyLGQBGRYFbG9jYWwxEzARBgoJkiaJk/IsZAEZ
+# FgN0amMxEjAQBgoJkiaJk/IsZAEZFgJhZDEbMBkGA1UEAxMSYWQtVzE2TUFJTkRD
+# UDAxLUNBMB4XDTE4MTAwODIxMDYwN1oXDTE5MTAwODIxMDYwN1owdDEVMBMGCgmS
+# JomT8ixkARkWBWxvY2FsMRMwEQYKCZImiZPyLGQBGRYDdGpjMRIwEAYKCZImiZPy
+# LGQBGRYCYWQxDjAMBgNVBAsTBXN0YWZmMQ4wDAYDVQQLEwVVU0VSUzESMBAGA1UE
+# AxMJQTAwMzAzMTQ5MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAt1qK
+# nm4sQhK8hymiP8M3P3G3uDkaFExk7RVADl4hQZiQSMmog2kkiKrqECzqp6ulhBJP
+# BVOWXLp3aBZCsslGcvLwR8eKZWhk2+7L5KdXKfb3Eo7jfZDQV/4SUi1cEuNZ6uu+
+# 5d8kpb+DZKxjwN7+u/7rK8q8Urnl44jiAh0gFx8r53/CHmkeYfYozIziI2yHsmiM
+# vH+WlvXBA7JH8pCoaK98CBm4Slyl9iCO6RXudtD5einnbOxePaUPM/xLcI7wCP3L
+# ppiKsJPyozGIK8MXOh4VsUDG/bq6r185vk8tV79OkHhUgaGvbOGi5xw5mJGleWkN
+# /c/FpN99iZhLbEBccwIDAQABo4ICajCCAmYwJQYJKwYBBAGCNxQCBBgeFgBDAG8A
+# ZABlAFMAaQBnAG4AaQBuAGcwEwYDVR0lBAwwCgYIKwYBBQUHAwMwDgYDVR0PAQH/
+# BAQDAgeAMB0GA1UdDgQWBBQq4YMEvaBKNG4Lj5YNmOg2vaOA9DAfBgNVHSMEGDAW
+# gBRunUTjaRhGdTlUbZ403p4E4033qjCB2QYDVR0fBIHRMIHOMIHLoIHIoIHFhoHC
+# bGRhcDovLy9DTj1hZC1XMTZNQUlORENQMDEtQ0EsQ049VzE2TUFJTkRDUDAxLENO
+# PUNEUCxDTj1QdWJsaWMlMjBLZXklMjBTZXJ2aWNlcyxDTj1TZXJ2aWNlcyxDTj1D
+# b25maWd1cmF0aW9uLERDPWFkLERDPXRqYyxEQz1sb2NhbD9jZXJ0aWZpY2F0ZVJl
+# dm9jYXRpb25MaXN0P2Jhc2U/b2JqZWN0Q2xhc3M9Y1JMRGlzdHJpYnV0aW9uUG9p
+# bnQwgcgGCCsGAQUFBwEBBIG7MIG4MIG1BggrBgEFBQcwAoaBqGxkYXA6Ly8vQ049
+# YWQtVzE2TUFJTkRDUDAxLUNBLENOPUFJQSxDTj1QdWJsaWMlMjBLZXklMjBTZXJ2
+# aWNlcyxDTj1TZXJ2aWNlcyxDTj1Db25maWd1cmF0aW9uLERDPWFkLERDPXRqYyxE
+# Qz1sb2NhbD9jQUNlcnRpZmljYXRlP2Jhc2U/b2JqZWN0Q2xhc3M9Y2VydGlmaWNh
+# dGlvbkF1dGhvcml0eTAxBgNVHREEKjAooCYGCisGAQQBgjcUAgOgGAwWQTAwMzAz
+# MTQ5QGFkLnRqYy5sb2NhbDANBgkqhkiG9w0BAQsFAAOCAQEAWsbGFusPT221Js4n
+# KfxPXkKWnIE439KRq59N+z3U38s7/Soi3GsnGmRSo2y2DgX2+2hJ6tlQk6ezaVfO
+# ssbRiry3hTs+ONDCX6oqNS2Amyj/on4KceMve22UZYqy5j+7kBeO1Ac4rxA/BWyU
+# gXScTSxfk08ZPjG75CSyKNsgOt8dUJyNWjxP4TaWCJ7q+EcxpO4oiHzceuPWEDr4
+# 7DGqAx0+oQwUhRAHeejaPuIWJGCaYEr1PpA372dFFbnOmrhUQVoJS56nbUFUxpb5
+# dcUAVrPx1hHIHiYXYyh4JJXYs6AeJ8rbdw1MQGSZUj0a8M72zeWwGM4DUh8gY5A9
+# AbgcATGCAhUwggIRAgEBMHQwXTEVMBMGCgmSJomT8ixkARkWBWxvY2FsMRMwEQYK
+# CZImiZPyLGQBGRYDdGpjMRIwEAYKCZImiZPyLGQBGRYCYWQxGzAZBgNVBAMTEmFk
+# LVcxNk1BSU5EQ1AwMS1DQQITFQAAByMkUpCwipe3DwAAAAAHIzAJBgUrDgMCGgUA
+# oHgwGAYKKwYBBAGCNwIBDDEKMAigAoAAoQKAADAZBgkqhkiG9w0BCQMxDAYKKwYB
+# BAGCNwIBBDAcBgorBgEEAYI3AgELMQ4wDAYKKwYBBAGCNwIBFTAjBgkqhkiG9w0B
+# CQQxFgQUp88ivAUM9H84c6VnSoIHy54SFhMwDQYJKoZIhvcNAQEBBQAEggEAS0pu
+# jIcyf42xbk6zl3bSaGbOgGVLkAM8y1lyr4mzJ5EBQgJ1C6AJODDWhal12KtZTLsX
+# +WYGNPw9hBCGcisVcrspGoBrPNeWRjy4zfMm7dClQU1SmXP8SnQ1G7m8YsnjfGFz
+# /zZBGooDCZgU5ZkKrLzvEoLWHnJ8xzS90GVGd/+M9WP91gYD1NoLKQukHbWTazl+
+# sNzi4oGF7h9zQXxkvTqH5t/p6lEDd5UzdeblXBODNF6vWie42yYmFzLyIl/5n9VB
+# R1rEAR0lLL5xL4vWxWdpCrrbB9fCk4Kv9lD3P6zRbM490nWlt/w6ragGvysXnLcj
+# 6CyFsmCvEctwzf9TwQ==
+# SIG # End signature block
