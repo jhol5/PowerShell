e6adfcf3f3113b85f0b98e99658e56ff94e89880
Joshua Holcomb <joshua.holcomb@tjc.edu>
2019-11-05 11:11:51 -0600

    Added script to restart ngt tools service

---
+++ Scripts/Restart-NutanixGuestTools.ps1
@@ -0,0 +1,15 @@
+﻿# ToDo:
+## Needs to be added to Nutanix.Guest.Tools module if not already added.
+
+$Credential = Get-Credential
+
+$Computers = @(
+    "computer 1",
+    "computer 2"
+)
+
+Foreach ($computer in $Computers) {
+    $computer_name = Invoke-Command -Credential $Credential -ScriptBlock { $env:COMPUTERNAME } -ComputerName $computer
+    Write-Host "Trying to restart Nutanix Agent on $computer_name" -ForegroundColor Green
+    Invoke-Command -Credential $Credential -ScriptBlock { Restart-Service -Name "Nutanix Guest Tools Agent" } -ComputerName $computer
+}
